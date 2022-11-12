18bc3494236d84d643d53e2e01dc27f957ca6669
Loc Tran <mrlocspoc@gmail.com>
2022-11-12 18:41:50 +1030

    301. Auth0 Setup complete

--- src/index.js
+++ src/index.js
@@ -5,12 +5,21 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { GithubProvider } from "./context/context";
 import { Auth0Provider } from "@auth0/auth0-react";
+const domain = `${process.env.AUTH0_DOMAIN}`;
+const clientId = `${process.env.AUTH0_CLIENTID}`;
 
+// ${process.env.AUTH0_DOMAIN};
 ReactDOM.render(
 	<React.StrictMode>
-		<GithubProvider>
-			<App />
-		</GithubProvider>
+		<Auth0Provider
+			domain={domain}
+			clientId={clientId}
+			redirectUri={window.location.origin}
+		>
+			<GithubProvider>
+				<App />
+			</GithubProvider>
+		</Auth0Provider>
 	</React.StrictMode>,
 	document.getElementById("root")
 );
